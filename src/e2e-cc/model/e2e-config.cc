8544359f4406d470244cf64d5af1d78eb92d5f61
Marvin Meiers <mmeiers@mpi-sws.org>
2024-04-08 20:31:04 +0200

    e2e-cc: add global probes

--- src/e2e-cc/model/e2e-config.cc
+++ src/e2e-cc/model/e2e-config.cc
@@ -366,6 +366,9 @@ E2EConfigParser::ParseArguments(int argc, char* argv[])
     m_cmd.AddValue("Probe",
                    "Add a probe to the simulation",
                    MakeBoundCallback(AddConfig, &m_probes));
+    m_cmd.AddValue("GlobalProbe",
+                   "Add a global probe to the simulation using config paths",
+                   MakeBoundCallback(AddConfig, &m_globalProbes));
     m_cmd.AddValue("Global", "Add global options", MakeBoundCallback(AddConfig, &m_globals));
     m_cmd.AddValue("ConfigFile", "A file that contains command line options", configFile);
     m_cmd.AddValue("Logging",
@@ -481,6 +484,12 @@ const std::vector<E2EConfig>&
 E2EConfigParser::GetProbeArgs()
 {
     return m_probes;
+}
+
+const std::vector<E2EConfig>&
+E2EConfigParser::GetGlobalProbeArgs()
+{
+    return m_globalProbes;
 }
 
 const std::vector<E2EConfig>&
--- src/e2e-cc/model/e2e-config.h
+++ src/e2e-cc/model/e2e-config.h
@@ -285,6 +285,7 @@ class E2EConfigParser
     const std::vector<E2EConfig>& GetNetworkArgs();
     const std::vector<E2EConfig>& GetApplicationArgs();
     const std::vector<E2EConfig>& GetProbeArgs();
+    const std::vector<E2EConfig>& GetGlobalProbeArgs();
     const std::vector<E2EConfig>& GetGlobalArgs();
     const std::vector<E2EConfig>& GetLoggingArgs();
 
@@ -300,6 +301,7 @@ class E2EConfigParser
     std::vector<E2EConfig> m_networks;
     std::vector<E2EConfig> m_applications;
     std::vector<E2EConfig> m_probes;
+    std::vector<E2EConfig> m_globalProbes;
     std::vector<E2EConfig> m_globals;
     std::vector<E2EConfig> m_logging;
 };
